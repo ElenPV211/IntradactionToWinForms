0df6ac4360c55ff692623ca5377d564c17085a7f
user <user@DESKTOP-LI69LEU>
2023-08-23 18:17:06 +0300

    Add project files.

---
+++ Intradaction/DateCalculator.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Intradaction
+{
+    partial class DateCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePickerStart = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePickerEnd = new System.Windows.Forms.DateTimePicker();
+            this.lblStartDate = new System.Windows.Forms.Label();
+            this.lblEndDate = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // dateTimePickerStart
+            // 
+            this.dateTimePickerStart.Location = new System.Drawing.Point(19, 49);
+            this.dateTimePickerStart.Name = "dateTimePickerStart";
+            this.dateTimePickerStart.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePickerStart.TabIndex = 0;
+            // 
+            // dateTimePickerEnd
+            // 
+            this.dateTimePickerEnd.Location = new System.Drawing.Point(309, 45);
+            this.dateTimePickerEnd.Name = "dateTimePickerEnd";
+            this.dateTimePickerEnd.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePickerEnd.TabIndex = 1;
+            // 
+            // lblStartDate
+            // 
+            this.lblStartDate.AutoSize = true;
+            this.lblStartDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblStartDate.Location = new System.Drawing.Point(13, 13);
+            this.lblStartDate.Name = "lblStartDate";
+            this.lblStartDate.Size = new System.Drawing.Size(145, 33);
+            this.lblStartDate.TabIndex = 2;
+            this.lblStartDate.Text = "Start Date";
+            // 
+            // lblEndDate
+            // 
+            this.lblEndDate.AutoSize = true;
+            this.lblEndDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblEndDate.Location = new System.Drawing.Point(303, 9);
+            this.lblEndDate.Name = "lblEndDate";
+            this.lblEndDate.Size = new System.Drawing.Size(135, 33);
+            this.lblEndDate.TabIndex = 4;
+            this.lblEndDate.Text = "End Date";
+            this.lblEndDate.UseMnemonic = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Years",
+            "Monthes",
+            "Weeks",
+            "Days",
+            "Hours",
+            "Minutes"});
+            this.comboBox1.Location = new System.Drawing.Point(19, 85);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(200, 21);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnCalculate.Location = new System.Drawing.Point(309, 82);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(200, 41);
+            this.btnCalculate.TabIndex = 6;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // DateCalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(544, 155);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.lblEndDate);
+            this.Controls.Add(this.lblStartDate);
+            this.Controls.Add(this.dateTimePickerEnd);
+            this.Controls.Add(this.dateTimePickerStart);
+            this.Name = "DateCalculator";
+            this.Text = "DateCalculate";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePickerStart;
+        private System.Windows.Forms.DateTimePicker dateTimePickerEnd;
+        private System.Windows.Forms.Label lblStartDate;
+        private System.Windows.Forms.Label lblEndDate;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnCalculate;
+    }
+}
---
+++ Intradaction/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Intradaction
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Location = new System.Drawing.Point
+                (
+                System.Windows.Forms.Screen.PrimaryScreen.Bounds.Width-this.Width-10,10
+                );
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // label1.Text = DateTime.Now.ToString();
+            string dateTime = DateTime.Now.ToString();
+            string[] dt = dateTime.Split(' ');//Split - разделитель
+            label1.Text = dt[1]+"\n";//вывод только времени на экран
+          //label1.Text = dt[1]+" "+dt[2];//конкантенация строк
+          //label1.Text = String.Format("{0} {1}", dt[0], dt[1]);//форматирование
+          //label1.Text = $"{dt[0]} {dt[1]}";//интерполяция строк  
+        if(cbShowDate.Checked)
+            {
+                label1.Text += dt[0];
+            }
+        }
+
+        private void label1_MouseHover(object sender, EventArgs e)
+        {
+           // this.FormBorderStyle = FormBorderStyle.Sizable;
+            
+            this.btnClose.Visible = true;
+            this.btnHideControls.Visible = true;
+            this.cbShowDate.Visible = true;
+            this.btnDateCalculator.Visible = true;
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();//это закрывает окно
+        }
+
+        private void btnHideControls_Click(object sender, EventArgs e)
+        {
+           // this.FormBorderStyle= FormBorderStyle.None;
+            this.btnClose.Visible = false;
+            this.btnHideControls.Visible = false;
+            this.cbShowDate.Visible = false;
+            this.btnDateCalculator.Visible = false;
+        }
+
+        private void btnDateCalculator_Click(object sender, EventArgs e)
+        {
+            DateCalculator dc = new DateCalculator{};
+            dc.ShowDialog(this);
+        }
+    }
+}
